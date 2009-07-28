06e902fab80d585f6eb295bf41cb756fbc2b8287
azer koculu <azer@kodfabrik.com>
2009-07-28 11:52:53 +0000

    added tabbar

--- content/uzbl-layout.js
+++ content/uzbl-layout.js
@@ -1,9 +1,11 @@
 var
-  uzbl_bottombar = null
+  uzbl_bottombar = null,
+  uzbl_tabbar = null
 ;
 
 function show_uzbl_ui(){
   uzbl_bottombar.style.display = 'block';
+  uzbl_tabbar.style.display = 'block';
 }
 
 function update_uzbl_ui(){
@@ -12,10 +14,12 @@ function update_uzbl_ui(){
 
 function hide_uzbl_ui(){
   uzbl_bottombar.style.display = 'none';
+  uzbl_tabbar.style.display = 'none';
 }
 
 function sel_uzbl_ui(){
   uzbl_bottombar = $('uzbl-bottombar');
+  uzbl_tabbar = $('uzbl-tabbar');
 }
 
 addEventListener('DOMContentLoaded',sel_uzbl_ui,false);
